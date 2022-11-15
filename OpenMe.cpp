4974104ca96f1ddafecfc478cdaf8c70568b531a
Joseph <75801386+joseph-468@users.noreply.github.com>
2022-11-15 12:46:59 +0000

    Update OpenMe.cpp

--- OpenMe.cpp
+++ OpenMe.cpp
@@ -8,7 +8,15 @@ int main() {
     ShowWindow(GetConsoleWindow(), SW_HIDE);
     ofstream file;
     file.open("vbscript.vbs");
-    file << "x=msgbox(\"Do you like men?\", 36, \"Possible Homosexual Detected\")\nIf x=6 then x=msgbox(\"I knew it.\", 48, \"Homosexual Detected\")\nIf x=7 then y=msgbox(\"Liar. I know you're gay.\", 48, \"Homosexual in Denial\")\nIf x=7 then y=msgbox(\"Last chance. Are you gay?\", 36, \"You better not lie.\")\nIf y=6 then x=msgbox(\"Thank you for admitting the truth.\", 48, \"Homosexual Detected\")\nIf y=7 then last=msgbox(\"Liar. We both know that you are gay.\", 48, \"Homosexual Detected\")\nDo While last=1\ny=msgbox(\"Liar. We both know that you are gay.\", 48, \"Homosexual Detected\")\nLoop";
+    file << "x=msgbox(\"Do you like men?\", 36, \"Possible Homosexual Detected\")\n\
+    If x=6 then x=msgbox(\"I knew it.\", 48, \"Homosexual Detected\")\n\
+    If x=7 then y=msgbox(\"Liar. I know you're gay.\", 48, \"Homosexual in Denial\")\n\
+    If x=7 then y=msgbox(\"Last chance. Are you gay?\", 36, \"You better not lie.\")\n\
+    If y=6 then x=msgbox(\"Thank you for admitting the truth.\", 48, \"Homosexual Detected\")\n\
+    If y=7 then last=msgbox(\"Liar. We both know that you are gay.\", 48, \"Homosexual Detected\")\n\
+    Do While last=1\n\
+    y=msgbox(\"Liar. We both know that you are gay.\", 48, \"Homosexual Detected\")\n\
+    Loop";
     file.close();
     system("wscript.exe vbscript.vbs");
     remove("vbscript.vbs");
